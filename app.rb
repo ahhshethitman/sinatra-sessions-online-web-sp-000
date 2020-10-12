a069c5d80adf1033ae43d3a212b5c5624709cb06
Hunter Sides <hunter.s.1337@gmail.com>
2020-10-12 22:06:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -58,7 +58,6 @@ class App < Sinatra::Base
   get '/finish' do
     "Hopefully that explains a little more about the concept of sessions.\nThe session is simply a way to store user data on a temporary basis.\nIn any web application, a user ID is typically used as a session ID.\nThis is because an ID attribute of a user is a unique identifier\nthat will always be distinguishable from other user ID attributes."
   end
-end
 
   get '/second_exercise' do
     "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
